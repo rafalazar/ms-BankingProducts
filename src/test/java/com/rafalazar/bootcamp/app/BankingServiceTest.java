e51ac0bbe87c2177a35c1aa63049bcebbc87fcf7
Jordan <jcr.salazar@outlook.com>
2020-02-05 10:15:45 -0500

    Se agregó una prueba unitaria

---
+++ src/test/java/com/rafalazar/bootcamp/app/BankingServiceTest.java
@@ -0,0 +1,48 @@
+package com.rafalazar.bootcamp.app;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.rafalazar.bootcamp.app.document.BankingProduct;
+import com.rafalazar.bootcamp.app.service.BankingProductService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BankingServiceTest {
+	
+	@Autowired
+	private WebTestClient client;
+	
+	@Autowired
+	private BankingProductService service;
+
+	@Test
+	void findAllBankingClients() {
+		client.get()
+		.uri("/bankingProduct/findAll")
+		.accept(MediaType.APPLICATION_JSON)
+		.exchange()
+		.expectStatus().isOk()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON)
+		.expectBodyList(BankingProduct.class)
+		.consumeWith(res -> {
+			List<BankingProduct> banking = res.getResponseBody();
+			banking.forEach(b -> {
+				System.out.println(b.getProductName() + " - " + b.getNameOwner());
+			});
+			
+			Assertions.assertThat(banking.size()>0).isTrue();
+		});
+	}
+
+}
